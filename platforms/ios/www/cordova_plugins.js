d722cb87e788393b5055479d32fa6e82d7dd029c
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2014-12-28 16:41:26 -0200

    Adding connection info plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -21,6 +21,21 @@ module.exports = [
         "clobbers": [
             "navigator.splashscreen"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -28,7 +43,8 @@ module.exports.metadata =
 {
     "org.apache.cordova.dialogs": "0.2.11",
     "org.apache.cordova.vibration": "0.3.12",
-    "org.apache.cordova.splashscreen": "0.3.5"
+    "org.apache.cordova.splashscreen": "0.3.5",
+    "org.apache.cordova.network-information": "0.2.14"
 }
 // BOTTOM OF METADATA
 });
